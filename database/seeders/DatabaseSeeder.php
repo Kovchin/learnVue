e60b45a3bd018aa5e302725132fa7b7c772df41c
Kovchin <kovchin@mail.ru>
2021-04-21 12:36:01 +0700

    Created admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        //generate admin
+        $adminUser = User::where('name', 'kovchin')->first();
+
+        if (!isset($adminUser)) {
+            User::firstOrCreate([
+                'name' => 'admin',
+                'email' => 'admin@admin.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('test1234'),
+                'remember_token' => Str::random(10),
+            ]);
+        }
+
         // \App\Models\User::factory(10)->create();
     }
 }
